63df01620f20349050327940d20246d4e71dbc1e
deafdead <102491831+0xdeafdead@users.noreply.github.com>
2025-01-06 09:16:33 -0600

    updated readme (#1)
    * updated readme
    
    * added ci
    
    * updated ci
    
    * added test step
    
    * added test step
    
    * added failed test intentionally to test ci
    
    * reverted failed unit test
    
    * added cover and reset cache command
    
    * added coverage badge to readme
    
    * added workflow name to the badge url

---
+++ internal/auth/auth_test.go
@@ -0,0 +1,64 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+
+func TestGetAPIKey(t *testing.T) {
+	tests := []struct {
+		name           string
+		headers        http.Header
+		expectedAPIKey string
+		expectedError  error
+	}{
+		{
+			name:           "Valid Authorization Header",
+			headers:        http.Header{"Authorization": {"ApiKey my-secret-key"}},
+			expectedAPIKey: "my-secret-key",
+			expectedError:  nil,
+		},
+		{
+			name:           "Missing Authorization Header",
+			headers:        http.Header{},
+			expectedAPIKey: "",
+			expectedError:  ErrNoAuthHeaderIncluded,
+		},
+		{
+			name:           "Malformed Authorization Header - Missing ApiKey",
+			headers:        http.Header{"Authorization": {"my-secret-key"}},
+			expectedAPIKey: "",
+			expectedError:  errors.New("malformed authorization header"),
+		},
+		{
+			name:           "Malformed Authorization Header - Empty ApiKey",
+			headers:        http.Header{"Authorization": {"ApiKey"}},
+			expectedAPIKey: "",
+			expectedError:  errors.New("malformed authorization header"),
+		},
+		{
+			name:           "Invalid Authorization Scheme",
+			headers:        http.Header{"Authorization": {"Bearer my-secret-key"}},
+			expectedAPIKey: "",
+			expectedError:  errors.New("malformed authorization header"),
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			apiKey, err := GetAPIKey(tt.headers)
+
+			if apiKey != tt.expectedAPIKey {
+				t.Errorf("expected API key %q, got %q", tt.expectedAPIKey, apiKey)
+			}
+
+			if (err == nil && tt.expectedError != nil) || (err != nil && tt.expectedError == nil) {
+				t.Errorf("expected error %v, got %v", tt.expectedError, err)
+			} else if err != nil && tt.expectedError != nil && err.Error() != tt.expectedError.Error() {
+				t.Errorf("expected error message %q, got %q", tt.expectedError.Error(), err.Error())
+			}
+		})
+	}
+}
